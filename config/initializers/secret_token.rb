ee4ece725b33b29866af9f4296abcf83cf323f38
Naoyuki Kumagai <kumagai@namaka>
2013-04-19 13:31:52 +0900

    First import with brand new Rails 3.2.8 files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hells::Application.config.secret_token = '2394594a35ab2a6e4fb3b4e48e54c5dfeeb48b44e7125f714f33a3a30eb39376fe42960dd2820ee70277ff8f31241c89b021e480dd85de5f77d51e41b74870cc'
